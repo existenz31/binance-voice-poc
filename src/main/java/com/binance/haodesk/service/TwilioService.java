0067f2b199d5a0ba5f3869b5e599039b45946171
Sebastien Paltrie <spaltrie@twilio.com>
2022-07-08 09:40:39 +0200

    Added fetch recording

--- src/main/java/com/binance/haodesk/service/TwilioService.java
+++ src/main/java/com/binance/haodesk/service/TwilioService.java
@@ -3,7 +3,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Call;
+import com.twilio.rest.api.v2010.account.*;
 import com.twilio.type.PhoneNumber;
 
 public class TwilioService {
@@ -12,8 +12,11 @@ public class TwilioService {
 
     public static void main(String[] args) throws URISyntaxException {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Recording recording =
+                Recording.fetcher("RE7bc9589c7efbb02f11d68de80ced5ce2")
+                        .fetch();
 
-        String from = "+15202631069";//"+15854763410";
+        String from = "+17753827206";//"+15854763410";
         String to = "+33631826839";
 
         Call call = Call.creator(new PhoneNumber(to), new PhoneNumber(from),
